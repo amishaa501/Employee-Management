5433120e0dea90d2d370ba0b62548af65132fdb0
Amisha <amisha.amisha@bounteous.com>
2024-08-27 19:44:52 +0530

    added all the backend endpoints

---
+++ backend/EmployeeManager/src/main/java/com/example/EmployeeManager/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.example.EmployeeManager.controller;
+
+import com.example.EmployeeManager.modal.User;
+import com.example.EmployeeManager.services.UserService;
+import com.example.EmployeeManager.services.UserServiceImplement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/addUser")
+    public String addUser(@RequestBody User user){
+       return userService.upsert(user);
+    }
+
+    @GetMapping("/getUserById/{id}")
+    public User getUser(@PathVariable Long id){
+        return userService.getUserById(id);
+    }
+
+    @GetMapping("/getAllUsers")
+    public List<User> getAllUsers(){
+        return userService.getAllUser();
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    public String deleteUser(@PathVariable Long id){
+        return userService.deleteById(id);
+    }
+
+
+}
